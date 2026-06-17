276ff9cce3a8a3b1b0258792b7735880143ae020
bsterea <bsterea@yahoo.com>
2026-06-17 19:19:49 +0200

    Add Django configuration for Items app

---
+++ backend/apps/items/apps.py
@@ -0,0 +1,13 @@
+from django.apps import AppConfig
+
+
+class ItemsConfig(AppConfig):
+    """
+    Django configuration for the Items app.
+    """
+
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "apps.items"
+
+
+# END OF FILE
