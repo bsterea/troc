8c850d5ab76c729b9795a1994c0a7123e01233a7
bsterea <bsterea@yahoo.com>
2026-06-17 19:25:22 +0200

    Configure media file serving in URL patterns
    Added static file serving for development environment.

--- backend/troc/urls.py
+++ backend/troc/urls.py
@@ -1,6 +1,9 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
 
+# Main URL configuration for the Troc project.
 urlpatterns = [
     path("", include("apps.core.urls")),
     path("admin/", admin.site.urls),
@@ -9,3 +12,9 @@ urlpatterns = [
     path("exchanges/", include("apps.exchanges.urls")),
     path("messages/", include("apps.messaging.urls")),
 ]
+
+# Serve uploaded media files during development.
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+# END OF FILE
