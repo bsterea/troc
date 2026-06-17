f1933fc8373a80f21c83365f77ea293995bf0f9a
bsterea <bsterea@yahoo.com>
2026-06-17 18:04:00 +0200

    Create UserProfileAdmin for managing user profiles
    Added admin configuration for UserProfile model.

--- backend/apps/items/admin.py
+++ backend/apps/items/admin.py
@@ -1,1 +1,47 @@
+from django.contrib import admin
 
+from .models import UserProfile
+
+
+@admin.register(UserProfile)
+class UserProfileAdmin(admin.ModelAdmin):
+    """
+    Admin configuration for Troc user profiles.
+    """
+
+    list_display = (
+        "id",
+        "phone_number",
+        "email",
+        "first_name",
+        "last_name",
+        "country",
+        "region",
+        "city",
+        "account_status",
+        "created_at",
+    )
+
+    list_filter = (
+        "account_status",
+        "country",
+        "region",
+        "city",
+        "created_at",
+    )
+
+    search_fields = (
+        "phone_number",
+        "email",
+        "first_name",
+        "last_name",
+        "country",
+        "region",
+        "city",
+    )
+
+    readonly_fields = (
+        "created_at",
+        "updated_at",
+        "deleted_at",
+    )
