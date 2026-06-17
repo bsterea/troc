4acb9bddbd9275b9cf39bfedc9984c8168aa15ef
bsterea <bsterea@yahoo.com>
2026-06-17 18:37:53 +0200

    Add main function to manage.py for Django commands

--- backend/manage.py
+++ backend/manage.py
@@ -1,1 +1,21 @@
+#!/usr/bin/env python
+import os
+import sys
 
+
+def main():
+    """Run administrative Django commands."""
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "troc.settings")
+
+    try:
+        from django.core.management import execute_from_command_line
+    except ImportError as exc:
+        raise ImportError(
+            "Could not import Django. Make sure it is installed and available."
+        ) from exc
+
+    execute_from_command_line(sys.argv)
+
+
+if __name__ == "__main__":
+    main()
