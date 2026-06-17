3f03bf08d943ec7fed14842f384cc9d6aa9654ba
bsterea <bsterea@yahoo.com>
2026-06-17 19:21:43 +0200

    Create CoreConfig for the Core app
    Added Django configuration for the Core app.

---
+++ backend/apps/core/apps.py
@@ -0,0 +1,13 @@
+from django.apps import AppConfig
+
+
+class CoreConfig(AppConfig):
+    """
+    Django configuration for the Core app.
+    """
+
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "apps.core"
+
+
+# END OF FILE
