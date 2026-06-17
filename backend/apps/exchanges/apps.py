fa1c2bcdfd9f8a4dce6e86cbdde9be5532650b16
bsterea <bsterea@yahoo.com>
2026-06-17 19:20:27 +0200

    Create ExchangesConfig for Exchanges app
    Added Django configuration for the Exchanges app.

---
+++ backend/apps/exchanges/apps.py
@@ -0,0 +1,13 @@
+from django.apps import AppConfig
+
+
+class ExchangesConfig(AppConfig):
+    """
+    Django configuration for the Exchanges app.
+    """
+
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "apps.exchanges"
+
+
+# END OF FILE
