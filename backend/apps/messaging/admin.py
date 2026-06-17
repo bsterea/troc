8c1199e6f7c42759d449e4e565bde4cbf5f78ac9
bsterea <bsterea@yahoo.com>
2026-06-17 18:05:03 +0200

    Create MessageAdmin for Message model in admin
    Added MessageAdmin class to manage Message model in admin.

--- backend/apps/messaging/admin.py
+++ backend/apps/messaging/admin.py
@@ -1,1 +1,39 @@
+from django.contrib import admin
 
+from .models import Message
+
+
+@admin.register(Message)
+class MessageAdmin(admin.ModelAdmin):
+    """
+    Admin configuration for user messages.
+    """
+
+    list_display = (
+        "id",
+        "exchange_offer",
+        "sender",
+        "receiver",
+        "created_at",
+        "read_at",
+    )
+
+    list_filter = (
+        "created_at",
+        "read_at",
+    )
+
+    search_fields = (
+        "sender__phone_number",
+        "sender__first_name",
+        "sender__last_name",
+        "receiver__phone_number",
+        "receiver__first_name",
+        "receiver__last_name",
+        "content",
+    )
+
+    readonly_fields = (
+        "created_at",
+        "read_at",
+    )
