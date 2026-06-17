ddf8c1cb07cfe07e87878fbedf3e03a3d061f21b
bsterea <bsterea@yahoo.com>
2026-06-17 19:21:06 +0200

    Add Django configuration for Messaging app

---
+++ backend/apps/messaging/apps.py
@@ -0,0 +1,13 @@
+from django.apps import AppConfig
+
+
+class MessagingConfig(AppConfig):
+    """
+    Django configuration for the Messaging app.
+    """
+
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "apps.messaging"
+
+
+# END OF FILE
